779e26d64dcde8ace53e31dbc1a87d41a790d78f
Depth-Hoar <44712760+Depth-Hoar@users.noreply.github.com>
2023-01-07 16:44:46 -0700

    goerli beresheet deployments

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const goerliPrivateKeys = process.env.GOERLI_PRIVATE_KEY || "";
+const berePrivateKeys = process.env.BERESHEET_PRIVATE_KEY || "";
 
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -20,6 +22,16 @@ module.exports = {
       gas: 2100000,
       gasPrice: 8000000000,
       gasLimit: 100
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: goerliPrivateKeys.split(','),
+      // accounts: [`0x${goerliPrivateKey}`],
+    },
+    beresheet: {
+      url: `https://beresheet-evm.jelliedowl.net`,
+      chainId: 2022,
+      accounts: berePrivateKeys.split(','),
     },
   }
 };
